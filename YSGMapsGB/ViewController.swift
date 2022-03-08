7bdf53ff8c5f776855449a87f387c09a43290df3
Jarek Šedý <jareksedy@icloud.com>
2022-03-08 13:08:58 +0600

    Adding markers to the map.

--- YSGMapsGB/ViewController.swift
+++ YSGMapsGB/ViewController.swift
@@ -10,29 +10,48 @@ import GoogleMaps
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let zoomValue: Float = 18
+    
     @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var locationButton: UIButton!
+    
+    private func addMarker(position: CLLocationCoordinate2D) {
+        let marker = GMSMarker(position: position)
+        marker.map = mapView
+    }
     
     private func configureMap() {
         let coordinate = CLLocationCoordinate2D(latitude: 43.238949, longitude: 76.889709)
-        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15)
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: zoomValue)
         mapView.camera = camera
     }
     
     private func configureLocationManager() {
-        let locationManager = CLLocationManager()
         locationManager.requestWhenInUseAuthorization()
         locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
     
-    @IBAction func updateLocation(_ sender: Any) {
+    @IBAction func locationButtonTapped(_ sender: Any) {
+        locationManager.requestLocation()
     }
     
-    @IBAction func currentLocation(_ sender: Any) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.first?.coordinate {
+            let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: zoomValue)
+            mapView.camera = camera
+            addMarker(position: coordinate)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureMap()
+        //configureMap()
         configureLocationManager()
     }
 
